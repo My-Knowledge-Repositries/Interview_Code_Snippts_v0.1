930eb76a1b337bfb1331dab323bfc819f1911995
oshadaera68 <mindartlk010@gmail.com>
2022-10-13 21:46:33 +0530

    String reverse class is completed

--- src/StringClass.java
+++
@@ -1,6 +0,0 @@
-// 1st interview question -
-public class StringClass {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/StringReverseClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// 1st java interview question - Reverse String for using scanner
+public class StringReverseClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String input = scanner.next();
+        System.out.println(reverse(input));
+    }
+
+    public static String reverse(String in) {
+        if (in == null) {
+            throw new RuntimeException("Null is a not valid...!!");
+        }
+        StringBuilder builder = new StringBuilder();
+        char[] chars = in.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            builder.append(chars[i]);
+        }
+        return builder.toString();
+    }
+
+}
